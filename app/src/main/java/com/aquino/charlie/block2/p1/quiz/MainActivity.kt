86f1f387b1af70f522d96d44f8d55b8d5cd437f5
Admin <chva.aquino.up@phinmaed.com>
2023-12-09 16:46:23 +0800

    11111

---
+++ app/src/main/java/com/aquino/charlie/block2/p1/quiz/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.aquino.charlie.block2.p1.quiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aquino.charlie.block2.p1.quiz.ui.theme.AquinoCharlieblock2QuizTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AquinoCharlieblock2QuizTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AquinoCharlieblock2QuizTheme {
+        Greeting("Android")
+    }
+}
